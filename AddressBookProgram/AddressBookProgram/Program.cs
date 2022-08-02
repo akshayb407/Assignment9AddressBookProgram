8f5d5df67af9a21efc870eefc1c32a495356b040
akshayb407 <akshayb407@gmail.com>
2022-08-03 01:03:57 +0530

    UC3_UpdateExistingContact

--- AddressBookProgram/AddressBookProgram/Program.cs
+++ AddressBookProgram/AddressBookProgram/Program.cs
@@ -6,15 +6,17 @@ namespace AddressBookProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Address Book Program!");
-            CreateContacts createContacts = new CreateContacts();
-            createContacts.CreateContact();
+            //Console.WriteLine("Welcome To Address Book Program!");
+            //CreateContacts createContacts = new CreateContacts();
+            //createContacts.CreateContact();
 
-            Console.WriteLine("Add New Contacts Program");
-            AddNewContacts addNewCon = new AddNewContacts();
-            addNewCon.addNewContacts();
+            // Console.WriteLine("Add New Contacts Program");
+            // AddNewContacts addNewCon = new AddNewContacts();
+            // addNewCon.addNewContacts();
 
-
+            Console.WriteLine("Update Existing Contact Program");
+            UpdateExistingContact update = new UpdateExistingContact();
+            update.UpdateContact();
         }
     }
 }
