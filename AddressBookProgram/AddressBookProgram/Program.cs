1fe3ace1c05a009dfc7bd4161a22adb98da28c73
akshayb407 <akshayb407@gmail.com>
2022-08-03 00:19:39 +0530

    .gitignore

---
+++ AddressBookProgram/AddressBookProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
